422432d0f7742a4b44ab254811f09495ae705a84
Zayn <mohamed99zayn@gmail.com>
2021-06-01 21:38:36 +0100

    test hexa number

--- app/Http/Controllers/fichierController.php
+++ app/Http/Controllers/fichierController.php
@@ -25,9 +25,12 @@ class fichierController extends Controller
 
     public function custom_encrypt(Request $request){
         $fichier = $request->file('fichier');
-        $key = hex2bin($request->input('key'));
-        $storedFile = Storage::putFileAS("/", $fichier, $fichier->getClientOriginalName());
-        (new FileVault)->key($key)->encryptCopy($storedFile);
-        return Storage::download($storedFile.".enc");
+        $keyhex= $request->input('key');
+        if(ctype_xdigit($keyhex) && strlen($keyhex)==64){
+            $keybin = hex2bin($keyhex);
+            $storedFile = Storage::putFileAS("/", $fichier, $fichier->getClientOriginalName());
+            (new FileVault)->key($keybin)->encryptCopy($storedFile);
+            return Storage::download($storedFile.".enc");
+        }else dd("no");
     }
 }
